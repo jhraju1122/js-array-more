a2cf37982c46339495c4308b399114d375137549
jhraju <jhraju890@gmail.com>
2024-02-03 09:35:30 +0600

    added js array and sort

---
+++ looping.js
@@ -0,0 +1,11 @@
+//looping technique
+// 1,for loof
+//2, while loof 
+// 3, do while 
+// 4, for of---> array loof korar jonno 
+// 5, for in -----> object loof korar jonno 
+
+const numbers = [1, 2, 3, 4, 5, 6, 7]
+console.log(numbers);
+numbers.reverse();  // a technique to reverse number & (unshift is another way)
+console.log(numbers);
---
+++ short.js
@@ -0,0 +1,4 @@
+//short //
+const numbers = [4,7,8,9,2,3,5];
+const numbers_asc = numbers.sort()
+console.log(numbers_asc);
